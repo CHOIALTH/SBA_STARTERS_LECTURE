a8ad83c10a2c86721a2de7f86616ae0f3475224d
CHOIALTH <dnjswns4545@gmail.com>
2022-12-08 17:48:43 +0900

    22-12-08 Lec Fin

---
+++ workspace_java/chap5/src/test/Test1.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	//별로 정삼각형(입력변수 값 = 줄개수) 찍기 
+	//ex) 입력변수 5= 1 3 5 7 9
+	
+	}
+
+}
---
+++ workspace_java/chap5/src/test/test2.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// 입력변수(최소5개~최대10개의 정수) -
+		//i)입력변수의 개수 구하기 ii) 오름차순(작은->큰) 정렬 결과 출력
+	}
+
+}
