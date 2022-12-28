f1d716a17d1c5d6582ce9f72d079314f6e4eff71
CHOIALTH <dnjswns4545@gmail.com>
2022-12-28 10:42:36 +0900

    22-12-28 Lec MariaDB1

--- SQL(MariaDB)/22-12-27 test.sql
+++ SQL(MariaDB)/22-12-27 test.sql
@@ -8,7 +8,9 @@ SELECT * FROM countries;
 -- 완 1. 80번부서의 평균급여보다 많은 급여를 받는 직원의 이름, 부서id, 급여를 조회하시오.
 SELECT FIRST_NAME, DEPARTMENT_ID, SALARY
 FROM employees 
-WHERE SALARY > (SELECT AVG(SALARY) FROM employees WHERE DEPARTMENT_ID = 50);
+WHERE SALARY > (SELECT AVG(SALARY) FROM employees WHERE DEPARTMENT_ID = 80); 
+-- 80번 부서의 평균(8955.~~~~)만 구하면 되니까 GROUP BY 사용할 필요X
+
 
 -- 완 2. 'South San Francisco'에 근무하는 직원의 최소급여보다 급여를 많이 받으면서 50 번부서의 평균급여보다 많은 급여를 받는 직원의 이름, 급여, 부서명,부서id를 조회하시오.
 SELECT FIRST_NAME, SALARY, DEPARTMENT_NAME, e.DEPARTMENT_ID
@@ -30,6 +32,14 @@ RIGHT OUTER JOIN jobs j
 ON e.job_id = j.job_id
 GROUP BY JOB_TITLE;
 
+SELECT JOB_TITLE, COUNT(*)
+FROM jobs j 
+LEFT OUTER JOIN employees e 
+ON j.job_id = e.job_id
+GROUP BY JOB_TITLE;
+
+-- 이 문제의 경우 사용되지 않은 직급이 없음
+
 
 -- 완 3-2. 직급별 인원수가 10명 이상인 직급만 출력결과에 포함시키시오.
 SELECT COUNT(j.job_id) AS 직급별인원수, JOB_TITLE AS 직급명, e.JOB_ID
@@ -38,6 +48,11 @@ RIGHT OUTER JOIN jobs j
 ON e.job_id = j.job_id
 GROUP BY JOB_ID
 HAVING COUNT(j.job_id) >= 10;
+
+SELECT job_title, COUNT(*)
+FROM jobs j LEFT OUTER JOIN employees e ON e.job_id = j.job_id
+GROUP BY job_title
+HAVING COUNT(*) >= 10;
 
 
 -- 4. 각 부서별 최대급여를 받는 직원의 이름, 부서명, 급여를 조회하시오.
@@ -50,6 +65,16 @@ GROUP BY e.department_id
 HAVING salary = MAX(salary)
 ;
 
+SELECT first_name AS "부서별 최대급여자", department_name, salary
+-- alias 별명 설정시 공백 사용하려면 "" 로 묶어주면 된다.
+FROM employees e
+JOIN departments d
+ON e.department_id = d.department_id
+WHERE (e.department_id, salary) in
+(SELECT department_id, MAX(salary) FROM employees GROUP BY department_id)
+;
+
+
 
 -- 5. 직원의 이름, 부서id, 급여를 조회하시오. 그리고 직원이 속한 해당 부서의 최소급여를 마지막에 포함시켜 출력 하시오.
 SELECT * FROM employees;
@@ -61,16 +86,48 @@ INNER JOIN employees e2
 ON e.department_id = e2.department_id
 GROUP BY DEPARTMENT_ID;
 
+SELECT first_name "직원 이름", department_id 부서코드, salary "내 급여", 
+(SELECT MIN(salary) FROM employees WHERE e.department_id = department_id) "소속 부서의 최소급여"
+-- 이경우 ()안에  GROUP BY department_id는 원하는 답 마주할 수 없음
+FROM employees e
+
+;
+
+
 
 -- 완  6. 월별 입사자 수를 조회하되, 입사자 수가 10명 이상인 월만 출력하시오.
-SELECT MONTH(HIRE_DATE)AS 월, COUNT(MONTH(HIRE_DATE)) AS 월별입사자수
+SELECT MONTH(HIRE_DATE) 월, COUNT(MONTH(HIRE_DATE)) "월별 입사자수"
 FROM employees 
 GROUP BY MONTH(HIRE_DATE)
 HAVING COUNT(MONTH(HIRE_DATE)) >= 10
 ;
 
+SELECT hire_month, COUNT(hire_month)
+FROM  (SELECT 
+case 
+-- 여기서 _ 는 각 5개 (xxxx- 영역이니까)
+when hire_date LIKE '_____01%' then '01'
+when hire_date LIKE '_____02%' then '02'
+when hire_date LIKE '_____03%' then '03'
+when hire_date LIKE '_____04%' then '04'
+when hire_date LIKE '_____05%' then '05'
+when hire_date LIKE '_____06%' then '06'
+when hire_date LIKE '_____07%' then '07'
+when hire_date LIKE '_____08%' then '08'
+when hire_date LIKE '_____09%' then '09'
+when hire_date LIKE '_____10%' then '10'
+when hire_date LIKE '_____11%' then '11'
+when hire_date LIKE '_____12%' then '12'
+END hire_month FROM employees) imsi
+-- imsi 라는 가상의 테이블에 hire_month 라는 컬럼을 담음
+GROUP BY hire_month
+HAVING COUNT(hire_month) >= 10
+ORDER BY 1;
+
+
+
 -- 완 7. 자신의 관리자(상사)보다 많은 급여를 받는 직원의 이름과 급여를 조회하시오.
-SELECT MY.first_name, MY.salary, BO.salary
+SELECT MY.first_name, MY.salary "내 급여", BO.salary "상사 급여"
 FROM employees MY
 LEFT OUTER JOIN employees BO
 ON MY.manager_id = BO.manager_id
@@ -102,14 +159,18 @@ JOIN countries c
 ON l.country_id = c.country_id 
 GROUP BY c.country_id
 HAVING COUNT(c.country_id) >= 3;
+-- HAVING COUNT(*) >= 3; 해도 상관없음
 
 -- 완 10. 직원의 폰번호, 이메일과 상사의 폰번호, 이메일을 조회하시오. 단, 상사가 없는 직원은 '<관리자 없음>'이 출력되도록 해야 한다.
 SELECT * FROM employees;
+
 SELECT MY.phone_number AS 직원폰번, MY.email AS 직원멜,
-BO.phone_number AS 상사폰번, BO.email AS 상사메일
+IFNULL(BO.phone_number, "<관리자 없음>") 상사폰번,
+IFNULL(BO.email, "<관리자 없음>") 상사이멜
 FROM employees MY
 LEFT OUTER JOIN employees BO
-ON MY.manager_id = BO.manager_id;
+-- 상사가 없는 직원 출력 위해 외부조인
+ON MY.manager_id = BO.employee_id;
 
 -- 완 11. 각 부서 이름별로 최대급여와 최소급여를 조회하시오. 단, 최대/최소급여가 동일한 부서는 출력결과에서 제외시킨다.
 SELECT department_name AS 부서명, e.department_id AS 부서ID,  MAX(e.salary) AS 최대급여, MIN(e.salary) AS 최소급여
@@ -118,6 +179,11 @@ JOIN departments d
 ON e.department_id = d.department_id
 GROUP BY e.department_id
 ;
+
+SELECT DEPARTMENT_NAME, MAX(SALARY), MIN(SALARY)
+FROM departments D JOIN employees E ON D.department_id = E.department_id
+GROUP BY DEPARTMENT_NAME
+HAVING MAX(SALARY) != MIN(SALARY);
 
 -- 12. 부서별, 직급별 평균급여를 조회하시오. 
 --    단, 평균급여가 50번부서의 평균보다 많은 부서만 출력되어야 합니다.
@@ -130,4 +196,11 @@ JOIN departments d
 ON e.department_id = d.department_id
 GROUP BY e.department_id, j.job_id
 -- HAVING 
+;
+
+SELECT DEPARTMENT_ID, JOB_ID, AVG(SALARY)
+FROM employees E
+GROUP BY DEPARTMENT_ID, JOB_ID
+HAVING AVG(SALARY) > (SELECT AVG(SALARY) FROM employees WHERE DEPARTMENT_ID = 50)
+-- SELECT AVG(SALARY) FROM employees WHERE DEPARTMENT_ID = 50 --> 3475.555556
 ;
