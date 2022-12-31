7a3259cd1151f55884ab2feaa7f11b8c924adf54
CHOIALTH <dnjswns4545@gmail.com>
2022-12-31 16:53:39 +0900

    221231 udemyjava1

---
+++ workspace_java/udemyJava/src/udemyJava/Lec221231.java
@@ -0,0 +1,18 @@
+package udemyJava;
+
+public class Lec221231 {
+	static void hello() {
+		// void : 리턴타입을 갖지 않는(리턴하는게 없는) 메서드
+		// 메서드 특징
+		// 1. 메서드를 불러낸다? = 그 메서드의 코드를 실행한다
+		// 2. 메서드 제목은 보통 소문자로 시작(자바가 대문자로 시작하는 것을 허용은 함..)
+		// 3. 변수 선언 규칙을 따른다 (키워드와 같은 이름 사용 불가.. camelCase 등등)
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		hello();
+	}
+
+}
