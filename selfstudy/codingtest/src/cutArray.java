009dca59c92e3e37ff43f7261a74d2d67f7692ef
CHOIALTH <dnjswns4545@gmail.com>
2023-01-13 23:32:50 +0900

    home setting init

---
+++ selfstudy/codingtest/src/cutArray.java
@@ -0,0 +1,9 @@
+public class cutArray {
+    // 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때,
+    // numbers의 num 1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        if
+        return answer;
+    }
+}
